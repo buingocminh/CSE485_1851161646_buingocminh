8062a61e883320b38071cb239f843010507c0893
buingocminh <63990524+buingocminh@users.noreply.github.com>
2020-07-27 21:25:32 +0700

    add
    thêm slide số 2

--- Bài Tập Lớn/js/main-script.js
+++ Bài Tập Lớn/js/main-script.js
@@ -1,10 +1,34 @@
-function Map1() {
-    // The location of Uluru
+$(document).ready(function(){
+  // slide#2
+  var carouselEl = $('.owl-carousel');
+  $("#btn-next").click(function () {
+    carouselEl.trigger('next.owl.carousel');
+  });
+  $("#btn-prev").click(function () {
+    carouselEl.trigger('prev.owl.carousel');
+  });
+  var owl = $('.owl-carousel');
+  owl.owlCarousel({
+    margin: 10,
+    nav: false,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 3
+      }
+    }
+  })
+  // map
+  function Map1() {
     var loc1 = {lat: 10.760803, lng: 106.661598};
-    // The map, centered at Uluru
     var map1 = new google.maps.Map(
         document.getElementById('map1'), {zoom: 16, center: loc1});
-    // The marker, positioned at Uluru
     var marker = new google.maps.Marker({position: loc1, map: map1});
   }
   function Map2() {
@@ -13,4 +37,6 @@ function Map1() {
         document.getElementById('map2'), {zoom: 50, center: loc2});
     var marker = new google.maps.Marker({position: loc2, map: map2});
   }
+})
+
  
