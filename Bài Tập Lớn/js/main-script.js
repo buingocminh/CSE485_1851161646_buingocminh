1eb5ec9b3b845baa0759a87561236a0c4affb8a6
buingocminh <63990524+buingocminh@users.noreply.github.com>
2020-08-12 21:05:16 +0700

    update
    responsive cho trang web

--- Bài Tập Lớn/js/main-script.js
+++ Bài Tập Lớn/js/main-script.js
@@ -25,20 +25,29 @@ $(document).ready(function () {
     }
   })
   // form event
-$('[name="btnRegister"]').click(function(){
-  var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-  if($('[name="Name"]').val()=="") alert("Vui lòng nhập 'Họ và tên' của bạn!")
-  else if($('[name="Birthday"]').val()=="") alert("Vui lòng nhập 'Ngày sinh' liên lạc!")
-  else if($('[name="Address"]').val()=="") alert("Vui lòng nhập 'Địa chỉ' liên lạc!")
-  else if($('[name="Phone"]').val()=="") alert("Vui lòng nhập 'Số điện thoại' liên lạc!")
-  else if(!($.isNumeric($('[name="Phone"]').val()))) alert("vui lòng nhập chính xac số điện thoại")
-  else if(!emailReg.test( $('[name="Email"]').val())) alert("vui lòng nhập chính xác email")
-  else if($('[name="Licence"]').val()=="") alert("lkjdhash")
-  else {
-    $("#complete").show()
-  }
-})
- 
+  $('[name="btnRegister"]').click(function () {
+    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    if ($('[name="Name"]').val() == "") alert("Vui lòng nhập 'Họ và tên' của bạn!")
+    else if ($('[name="Birthday"]').val() == "") alert("Vui lòng nhập 'Ngày sinh' liên lạc!")
+    else if ($('[name="Address"]').val() == "") alert("Vui lòng nhập 'Địa chỉ' liên lạc!")
+    else if ($('[name="Phone"]').val() == "") alert("Vui lòng nhập 'Số điện thoại' liên lạc!")
+    else if (!($.isNumeric($('[name="Phone"]').val()))) alert("vui lòng nhập chính xac số điện thoại")
+    else if (!emailReg.test($('[name="Email"]').val())) alert("vui lòng nhập chính xác email")
+    else if ($('[name="Licence"]').val() == "") alert("lkjdhash")
+    else {
+      $("#complete").show()
+    }
+  })
+  // menu responsive
+      $(".navbar-toggler").on('click', function(){
+        if($(".menu-overlay").css('display')=='none')
+        $(".menu-overlay").fadeIn(500)
+        else $(".menu-overlay").fadeOut(500);
+    })
+    $(".menu-overlay").click(function(event) {
+        $(".navbar-toggler").trigger("click");
+        $(".menu-overlay").fadeOut(500);
+    }); 
   // go top
   $(window).on("scroll", function () {
     if ($(window).scrollTop() > 500) {
@@ -49,12 +58,12 @@ $('[name="btnRegister"]').click(function(){
     }
   });
   $('#btn-gotop').click(function () {
-    $('html, body').animate({scrollTop:0}, 'slow');
+    $('html, body').animate({ scrollTop: 0 }, 'slow');
   })
 
 })
- // map
- function Map1() {
+// map
+function Map1() {
   var loc1 = { lat: 10.760803, lng: 106.661598 };
   var map1 = new google.maps.Map(
     document.getElementById('map1'), { zoom: 16, center: loc1 });
